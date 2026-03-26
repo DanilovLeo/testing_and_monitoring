bf80f7ac34a343297ceaebe95ea2a961483d6cb1
LeoDanilov <danilovleo18@gmail.com>
2026-03-26 14:15:01 +0000

    remove temp files

--- check_model.py
+++
@@ -1,6 +0,0 @@
-import mlflow
-mlflow.set_tracking_uri('http://158.160.2.37:5000/')
-client = mlflow.tracking.MlflowClient()
-artifacts = client.list_artifacts('49dbed45ad1a4e889ab467482facbf00')
-for a in artifacts:
-    print(a.path, a.is_dir)
